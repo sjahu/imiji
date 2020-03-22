91785f9ec06792b011d7f338b64f6f9dfe4c052d
Stephen Humphries <stephen@example.com>
2020-03-22 14:50:01 -0400

    Added git ignore and gallery create call + some changes to image calls

--- src/imiji/__init__.py
+++ src/imiji/__init__.py
@@ -1,5 +1,7 @@
 from flask import *
 from datetime import datetime
+from base64 import b64encode, b64decode
+from io import BytesIO
 
 from db import DB_Handler
 
@@ -25,29 +27,74 @@ def create_app(test_config=None):
         Upload an image. Request body must be json:
 
         {
-          file: ... (REQUIED),
+          file: ... (REQUIED) (base64 encoded),
           description: ... (OPTIONAL)
         }
         """
         if not request.json or 'file' not in request.json:
             abort(400) # bad request
         try:
-            id = db.insert_image(file = request.json['file'],
-                                   description = request.json.get('description', ''),
-                                   upload_date = datetime.now(),
-                                   uploader_ip = request.remote_addr)
+            id = db.insert_image(file = b64decode(request.json['file']),
+                                 description = request.json.get('description', ''),
+                                 date = datetime.now(),
+                                 uploader_ip = request.remote_addr)
             return make_response(jsonify({'id': id}), 201) # created
         except Exception as e:
             return make_response(jsonify({'error': 'Failed to upload image', 'details': str(e)}), 500)
 
-    @app.route('/api/v1.0/image/<id>')
+    @app.route('/api/v1.0/image/<id>', methods=['GET'])
     def get_image(id):
         """
         Retrieve an image by its ID
+
+        Returns json response { id, file, description, upload_date }
         """
-        data = db.select_image(id)
-        if data == None:
-            abort(404)
-        return make_response(jsonify(data), 200)
+        try:
+            data = db.select_image(id)
+            data['file'] = b64encode(data['file']).decode() # encode as b64 then convert to string
+            if data == None:
+                abort(404)
+            return make_response(jsonify(data), 200)
+        except Exception as e:
+            return make_response(jsonify({'error': 'Failed to get image', 'details': str(e)}), 500)
+
+    @app.route('/api/v1.0/image/<id>.jpg', methods=['GET'])
+    def get_image_raw(id):
+        """
+        Retrieve an image by its ID
+
+        Return only the file itself
+        """
+        try:
+            data = db.select_image(id)
+            if data == None:
+                abort(404)
+
+            return send_file(BytesIO(data['file']), attachment_filename=f'{id}.jpg')
+        except Exception as e:
+            return make_response(jsonify({'error': 'Failed to get image', 'details': str(e)}), 500)
+
+    @app.route('/api/v1.0/gallery/create', methods=['POST'])
+    def create_gallery():
+        """
+        Create an image gallery
+
+        Request body must be json:
+
+        {
+          title: ... (OPTIONAL),
+          images: ... (OPTIONAL)
+        }
+        """
+        if not request.json:
+            abort(400) # bad request
+        try:
+            id = db.insert_gallery(title = request.json.get('title', ''),
+                                 date = datetime.now(),
+                                 images = request.json.get('images', []),
+                                 uploader_ip = request.remote_addr)
+            return make_response(jsonify({'id': id}), 201) # created
+        except Exception as e:
+            return make_response(jsonify({'error': 'Failed to upload image', 'details': str(e)}), 500)
 
     return app
--- src/imiji/db.py
+++ src/imiji/db.py
@@ -1,7 +1,6 @@
 from pymongo import MongoClient
 from gridfs import GridFS
 import utils
-from base64 import b64encode, b64decode
 
 class DB_Handler:
     def __init__(self, config):
@@ -17,34 +16,60 @@ class DB_Handler:
         self.client = MongoClient(config['MONGO_URI'])
         self.db = self.client.get_default_database() # get db specified in uri
         self.images = self.db['images'] # get images collection
+        self.galleries = self.db['galleries'] # get images collection
         self.fs = GridFS(self.db)
 
-    def insert_image(self, file, description, upload_date, uploader_ip):
+    def insert_image(self, file, description, date, uploader_ip):
         """
         Insert an image document and return the id
+
+        file should be bytes
         """
         doc = { 'id': self.gen_unique_id(), # note this is different from Mongo's _id field
-                'file': self.fs.put(file.encode()),
+                'file': self.fs.put(file),
                 'description': description,
-                'upload_date': upload_date,
+                'date_created': date,
                 'uploader_ip': uploader_ip
               }
         self.images.insert_one(doc)
         return doc['id']
 
     def gen_unique_id(self):
+        """
+        Generate a unique alphanumeric id for the image
+
+        The length of the id is controlled by self.id_size, set during class init by ID_SIZE config property.
+        """
         for _ in range(5):
             id = utils.random_id(self.id_size)
-            if self.images.find_one({ 'id': id }) == None:
+            if self.images.find_one({ 'id': id }) == None and \
+               self.galleries.find_one({ 'id': id }) == None:
                 return id
         raise Exception("Failed to generate unique ID!")
 
     def select_image(self, id):
         """
         Select and return an image
+
+        file is returned as bytes
         """
         doc = self.images.find_one({ 'id': id })
         if doc == None:
             return None
-        file = b64encode(self.fs.get(doc['file']).read()).decode() # read file and convert to b64
+        file = self.fs.get(doc['file']).read()
         return { 'id': id, 'file': file, 'description': doc['description'], 'upload_date': doc['upload_date']}
+
+    def insert_gallery(self, title, date, images, uploader_ip):
+        """
+        Insert a gallery document and return the id
+
+        images is a list of image IDs
+        """
+        doc = { 'id': self.gen_unique_id(),
+                'title': title,
+                'date_created': date,
+                'images': images,
+                'uploader_ip': uploader_ip
+              }
+        self.galleries.insert_one(doc)
+        return doc['id']
