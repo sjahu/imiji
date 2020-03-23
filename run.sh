e0cb81d5444fdb3835c849d9071a096f8fe1f602
Stephen Humphries <stephen@example.com>
2020-03-23 01:46:42 -0400

    Directory re-org

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 export FLASK_APP=src/imiji
 export FLASK_ENV=development # disable in production
-export PYTHONPATH=${PYTHONPATH}:$(pwd)/src/imiji # add to path so __init__ can find other files
+export PYTHONPATH=${PYTHONPATH}:$(pwd)/imiji # add to path so __init__ can find other files
 flask run
---
+++ test/README.md
@@ -0,0 +1,4 @@
+# Test
+Testing documentation goes here.
+
+I didn't get to this, but I need unit tests for the database handler and for the REST API.
